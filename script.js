787c50de44a955fa117677db28599f3b755d705e
Anthonyph <anthony.phim@gmail.com>
2019-11-18 11:50:03 +0100

    fonctionnalité 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// fonctionnalité 1 
 
 var footer = document.querySelector("footer.text-muted");
 let compteur = 0
@@ -8,6 +9,38 @@ function f1(){
   console.log("clique n°" + compteur);
   };
 
+// fonctionnalité 2 
+var navbar = document.getElementById("navbarHeader");
+  
+var buttonnavbar = document.getElementsByClassName("navbar-toggler");
+
+console.log(buttonnavbar[0]);
+
+let compteur2 = 0;
+
+buttonnavbar[0].addEventListener('click', f2, true);
+
+  function f2(){
+
+    if (navbar.className == ("collapse bg-dark")) {
+      navbar.classList.toggle("collapse");
+      console.log(navbar.className)
+    }
+    else {
+      navbar.classList.toggle("collapse");
+      console.log(navbar.className);
+    }
+
+    compteur2 ++;
+    console.log("clique n°" + compteur2);
+
+  };
+
+// fonctionnalité 3
+
+
+// fonctionnalité 4
 
 
 
+// fonctionnalité 5 
